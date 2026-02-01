3b17fecbffdbf627fb5d2cfb08127252aaf19de1
varunsvs71-ux <varuns.vs71@gmail.com>
2026-02-01 19:00:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const yesBtn = document.getElementById("yes");
-  const noBtn = document.getElementById("no");
-  const message = document.getElementById("message");
+const yesBtn = document.getElementById("yes");
+const noBtn = document.getElementById("no");
+const message = document.getElementById("message");
 
-  yesBtn.addEventListener("click", () => {
-    message.innerText = "Yayyy 💖 I knew it! Happy Valentine’s Day 😘";
-  });
+yesBtn.addEventListener("click", () => {
+    message.innerText = "Yay! I knew you would say YES ❤️🥰";
+});
 
-  function moveNoButton() {
+noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * 300 - 150;
-    const y = Math.random() * 200 - 100;
+    const y = Math.random() * 300 - 150;
     noBtn.style.transform = `translate(${x}px, ${y}px)`;
-  }
-
-  // Runs on hover
-  noBtn.addEventListener("mouseover", moveNoButton);
-
-  // Runs on click (important)
-  noBtn.addEventListener("click", moveNoButton);
 });
