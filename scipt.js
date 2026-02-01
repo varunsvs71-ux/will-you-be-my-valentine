77022edf7737f1dba6b8f5e9878f00ea6d404134
varunsvs71-ux <varuns.vs71@gmail.com>
2026-02-01 18:27:02 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,13 +1,15 @@
-const yesBtn = document.getElementById("yes");
-const noBtn = document.getElementById("no");
-const message = document.getElementById("message");
+document.addEventListener("DOMContentLoaded", () => {
+  const yesBtn = document.getElementById("yes");
+  const noBtn = document.getElementById("no");
+  const message = document.getElementById("message");
 
-yesBtn.addEventListener("click", () => {
-  message.innerText = "Yay! 💕 I knew it! Happy Valentine’s Day 😘";
-});
+  yesBtn.addEventListener("click", () => {
+    message.innerText = "Yay! 💕 I knew it! Happy Valentine’s Day 😘";
+  });
 
-noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * 200 - 100;
-  const y = Math.random() * 200 - 100;
-  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+  noBtn.addEventListener("mouseover", () => {
+    const x = Math.random() * 200 - 100;
+    const y = Math.random() * 200 - 100;
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+  });
 });
